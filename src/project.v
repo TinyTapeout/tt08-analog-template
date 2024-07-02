491f5534a7bbb5b064ec6581cb18d0b2bbb7c133
matt venn <matt@mattvenn.net>
2024-07-02 18:07:54 +0200

    Change VPWR to VDPWR and add a commented out VAPWR

--- src/project.v
+++ src/project.v
@@ -7,7 +7,8 @@
 
 module tt_um_example (
     input  wire       VGND,
-    input  wire       VPWR,
+    input  wire       VDPWR,    // 1.8v power supply
+//    input  wire       VAPWR,    // 3.3v power supply
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
